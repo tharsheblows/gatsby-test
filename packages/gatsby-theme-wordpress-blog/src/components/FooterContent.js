45a166539b4622cd5a6216e83ff20a6f04b4c0d4
JJ <jjjay@mac.com>
2019-09-15 10:57:54 +0100

    add: theme acknowledgment

--- packages/gatsby-theme-wordpress-blog/src/components/FooterContent.js
+++ packages/gatsby-theme-wordpress-blog/src/components/FooterContent.js
@@ -6,6 +6,11 @@ const FooterContent = () => {
       © {new Date().getFullYear()} | Built with{` `}
       <a href="https://www.wpgraphql.com">WPGraphQL</a> and{` `}
       <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <br />
+      Theme shadowed:{' '}
+      <a href="https://www.npmjs.com/package/@alexadark/gatsby-theme-wordpress-blog">
+        Gatsby Theme WordPress Blog by Alexandra Dark
+      </a>
     </>
   )
 }
